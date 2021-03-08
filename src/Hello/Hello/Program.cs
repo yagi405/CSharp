e3f6553355d179970371cb5b1ca11ed149880c02
h-saito <h-saito@powersolutions.co.jp>
2021-03-08 20:25:10 +0900

    add Hello project

---
+++ src/Hello/Hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
