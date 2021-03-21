1788bb4f4c2acf38597b03903a9d000e6b96ec06
h-saito <h-saito@powersolutions.co.jp>
2021-03-21 12:50:42 +0900

    add VariableOperator exercise projects

---
+++ src/VariableOperator/VariableOperator.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VariableOperator.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            short s = 200;
+            double d = 0.001;
+
+            //暗黙の型変換
+            double result = d + i / s;
+            Console.WriteLine(result);
+
+            //明示的型変換
+            //s = i;
+            s = (short)i;
+
+            int a = 100;
+            int b = 5;
+            Console.WriteLine("result: {0}", a + b);
+
+            //ビットシフト
+            int c = 6;
+            Console.WriteLine(c >> 1);
+            Console.WriteLine(c << 1);
+
+            //ビット演算
+            bool x = false;
+            bool y = true;
+            Console.WriteLine(x ^ y);
+        }
+    }
+}
---
+++ src/VariableOperator/VariableOperator.Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VariableOperator.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("左辺の整数を入力してください。=> ");
+            string leftSide = Console.ReadLine();
+            Console.Write("左辺の整数を入力してください。=> ");
+            string rightSide = Console.ReadLine();
+
+            int a = int.Parse(leftSide);
+            int b = int.Parse(rightSide);
+            */
+
+            Console.Write("左辺の数値を入力してください。=> ");
+            string leftSide = Console.ReadLine();
+            Console.Write("左辺の数値を入力してください。=> ");
+            string rightSide = Console.ReadLine();
+
+            double a = double.Parse(leftSide);
+            double b = double.Parse(rightSide);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
