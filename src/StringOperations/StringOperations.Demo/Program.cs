97d73f9307db1e0f1c6b34554d52ad5fc2118ea9
h-saito <h-saito@powersolutions.co.jp>
2021-03-29 21:28:19 +0900

    add StringOperations exercise project

---
+++ src/StringOperations/StringOperations.Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace StringOperations.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Format();
+            //Escape();
+            //HereDocument();
+            StringInterpolation();
+        }
+
+        private static void Format()
+        {
+            var input = Console.ReadLine();
+
+            //Console.WriteLine("Hello {0}!",input);
+            var result = string.Format("Hello {0}!", input);
+            Console.WriteLine(result);
+        }
+
+        private static void Escape()
+        {
+            var s1 = "1行目\r\n2行目\r\n3行目";
+            Console.WriteLine(s1);
+
+            var s2 = "\\";
+            Console.WriteLine(s2);
+
+            var s3 = "改行文字 => \\r\\n";
+            Console.WriteLine(s3);
+        }
+
+        private static void HereDocument()
+        {
+            var s = @"1行目
+2行目
+3行目
+                    4行目
+";
+            Console.WriteLine(s);
+        }
+
+        private static void StringInterpolation()
+        {
+            var s = "aBc";
+
+            //Console.WriteLine("長さ => {0}",s.Length);
+            Console.WriteLine($"長さ => {s.Length}");
+
+            var upper = $"全て大文字 => {s.ToUpper()}";
+            Console.WriteLine(upper);
+        }
+    }
+}
---
+++ src/StringOperations/StringOperations.Exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StringOperations.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                Console.WriteLine("長さ => {0}", input.Length);
+                Console.WriteLine("すべて大文字 => {0}", input.ToUpper());
+                Console.WriteLine("すべて小文字 => {0}", input.ToLower());
+
+                var rev = "";
+                for (var i = input.Length - 1; i >= 0; i--)
+                {
+                    var s = input.Substring(i, 1);
+                    rev += s;
+                }
+                Console.WriteLine("逆順 => {0}", rev);
+
+                var inv = "";
+                for (var i = input.Length - 1; i >= 0; i--)
+                {
+                    var s = input.Substring(i, 1);
+
+                    var upper = s.ToUpper();
+                    if (s == upper)
+                    {
+                        inv += s.ToLower();
+                    }
+                    else
+                    {
+                        inv += s.ToUpper();
+                    }
+                }
+                Console.WriteLine("逆順+反転 => {0}",inv);
+                Console.WriteLine();
+            }
+        }
+    }
+}
