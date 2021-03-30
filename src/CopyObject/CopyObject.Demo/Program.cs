58df61a6abaa9aee28417247d6188d8ad9d97775
h-saito <h-saito@powersolutions.co.jp>
2021-03-30 21:28:00 +0900

    add CopyObject demo project

---
+++ src/CopyObject/CopyObject.Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CopyObject.Demo
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var src = new[] {1, 2, 3};
+            var dest = new int[src.Length];
+
+            for (var i = 0; i < src.Length; i++)
+            {
+                dest[i] = src[i];
+            }
+
+            src[0] = 0;
+            for (var i = 0; i < dest.Length; i++)
+            {
+                Console.WriteLine($"dest[{i}] => {dest[i]}");
+            }
+        }
+    }
+}
