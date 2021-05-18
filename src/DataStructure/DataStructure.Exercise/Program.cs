a32096352bdaf674fb7238b51d09fb779b7c7fc7
hiroki-saito <h-saito@powersolutions.co.jp>
2021-05-18 20:55:05 +0900

    add DataStructure project

---
+++ src/DataStructure/DataStructure.Exercise/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new VariableLengthArray();
+
+            PrintVariableLengthArray(array);
+
+            //Console.WriteLine(array[0]); //error
+
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            //1 2 3
+            PrintVariableLengthArray(array);
+            
+            array.AddRange(new[] { 4, 5, 6 });
+            
+            //1 2 3 4 5 6
+            PrintVariableLengthArray(array);
+
+            array.RemoveLast();
+
+            //1 2 3 4 5
+            PrintVariableLengthArray(array);
+
+            array.RemoveAt(2);
+
+            //1 2 4 5
+            PrintVariableLengthArray(array);
+
+            array.Insert(2, 30);
+            array.Insert(0, 0);
+
+            //0 1 2 30 4 5
+            PrintVariableLengthArray(array);
+
+            var array2 = new VariableLengthArray(0);
+            array2.Add(0);
+            array2.Add(1);
+            array2.Add(2);
+            //0 1 2
+            PrintVariableLengthArray(array2);
+
+            array2.Clear();
+            PrintVariableLengthArray(array2);
+
+        }
+
+        private static void PrintVariableLengthArray(VariableLengthArray array)
+        {
+            Console.WriteLine($"Count => {array.Count}");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine($"array[{i}] => {array[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/DataStructure/DataStructure.Exercise/VariableLengthArray.cs
@@ -0,0 +1,130 @@
+﻿namespace DataStructure.Exercise
+{
+    public class VariableLengthArray
+    {
+        private int[] _items;
+
+        private const int DefaultCapacity = 4;
+
+        public int Count { get; private set; }
+
+        public VariableLengthArray() : this(DefaultCapacity) { }
+
+        public VariableLengthArray(int capacity)
+        {
+            _items = new int[capacity];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                RaiseErrorIfIndexOutOfRange(index);
+                return _items[index];
+            }
+            set
+            {
+                RaiseErrorIfIndexOutOfRange(index);
+                _items[index] = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            EnsureCapacity();
+            _items[Count] = value;
+            Count++;
+        }
+
+        public void AddRange(params int[] values)
+        {
+            if (values == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Add(values[i]);
+            }
+        }
+
+        public void Insert(int index, int value)
+        {
+            RaiseErrorIfIndexOutOfRange(index);
+            EnsureCapacity();
+
+            for (int i = Count; i > index; i--)
+            {
+                _items[i] = _items[i - 1];
+            }
+
+            _items[index] = value;
+
+            Count++;
+        }
+
+        public void RemoveLast()
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+            Count--;
+        }
+
+        public void RemoveAt(int index)
+        {
+            RaiseErrorIfIndexOutOfRange(index);
+
+            for (int i = index; i < _items.Length - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            Count--;
+        }
+
+        public void Clear()
+        {
+            _items = new int[DefaultCapacity];
+            Count = 0;
+        }
+
+        private void RaiseErrorIfIndexOutOfRange(int index)
+        {
+            if (Count <= index)
+            {
+                _ = _items[_items.Length];
+            }
+        }
+
+        private void EnsureCapacity()
+        {
+            if (_items.Length != Count)
+            {
+                return;
+            }
+            Grow();
+        }
+
+        private void Grow()
+        {
+            int capacity;
+            if (_items.Length > DefaultCapacity)
+            {
+                capacity = _items.Length;
+            }
+            else
+            {
+                capacity = DefaultCapacity;
+            }
+
+            var items = new int[capacity * 2];
+            for (int i = 0; i < _items.Length; i++)
+            {
+                items[i] = _items[i];
+            }
+            _items = items;
+        }
+    }
+}
