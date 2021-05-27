347605f852b3f0d5a4fec6968cf2c9ab9ad70b49
hiroki-saito <h-saito@powersolutions.co.jp>
2021-05-27 10:15:08 +0900

    add StaticClass project

---
+++ src/StaticClass/StaticClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var max = Math.Max(100,150);
+            //var math = new Math();
+        }
+    }
+}
---
+++ src/StaticClass/StaticClass/Sample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public class Sample
+    {
+        public const string ConstField = "const value";
+
+        private string _field;
+
+        private static string _staticField;
+
+        public string Property { get; set; }
+
+        public static string StaticProperty { get; set; }
+
+        public Sample()
+        {
+            _field = "value";
+            Console.WriteLine("Constructor");
+        }
+
+        static Sample()
+        {
+            _staticField = "static value";
+            Console.WriteLine("StaticConstructor");
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("StaticMethod");
+        }
+    }
+
+    public static class StaticSample
+    {
+        public const string ConstField = "const value";
+
+        //private string _field;
+
+        private static string _staticField;
+
+        //public string Property { get; set; }
+
+        public static string StaticProperty { get; set; }
+
+        //public StaticSample()
+        //{
+        //    _field = "value";
+        //    Console.WriteLine("Constructor");
+        //}
+
+        static StaticSample()
+        {
+            _staticField = "static value";
+            Console.WriteLine("StaticConstructor");
+        }
+
+        //public void Method()
+        //{
+        //    Console.WriteLine("Method");
+        //}
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("StaticMethod");
+        }
+    }
+}
