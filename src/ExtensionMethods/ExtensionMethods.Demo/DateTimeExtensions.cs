39e54932d9343a97b9bb074ce7958c1a58738f1e
hiroki-saito <h-saito@powersolutions.co.jp>
2021-05-27 10:32:08 +0900

    add ExtensionMethods project

---
+++ src/ExtensionMethods/ExtensionMethods.Demo/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethods.Demo
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime BeginOfMonth(this DateTime self)
+        {
+            return new DateTime(self.Year, self.Month, 1);
+        }
+    }
+}
---
+++ src/ExtensionMethods/ExtensionMethods.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethods.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "123";
+            var i = s.ParseInt();
+            Console.WriteLine(i * 2);
+
+            var now = DateTime.Now;
+            Console.WriteLine(now.BeginOfMonth());
+            Console.WriteLine(DateTimeExtensions.BeginOfMonth(now));
+        }
+    }
+}
