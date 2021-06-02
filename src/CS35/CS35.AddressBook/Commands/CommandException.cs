bbb91ae3d0c06c7b0ffd630b1cb42f17eb5796d7
hiroki-saito <h-saito@powersolutions.co.jp>
2021-06-02 20:54:24 +0900

    add comment

--- src/CS35/CS35.AddressBook/Commands/CommandException.cs
+++ src/CS35/CS35.AddressBook/Commands/CommandException.cs
@@ -1,20 +1,30 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace CS35.AddressBook.Commands
 {
     /// <summary>
     /// コマンド操作時の例外を示すクラスです。
     /// </summary>
-    [Serializable]
     public class CommandException : Exception
     {
+        /// <summary>
+        /// <see cref="CommandException"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
         public CommandException() { }
 
-        public CommandException(string message) : base(message) { }
+        /// <summary>
+        /// 指定したエラーメッセージを使用して、<see cref="CommandException"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="message">エラーを説明するメッセージ</param>
+        public CommandException(string message)
+            : base(message) { }
 
-        public CommandException(string message, Exception innerException) : base(message, innerException) { }
-
-        protected CommandException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        /// <summary>
+        /// 指定したエラーメッセージおよびこの例外の原因となった内部例外への参照を使用して、<see cref="CommandException"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="message">例外の原因を説明するエラーメッセージ</param>
+        /// <param name="innerException">現在の例外の原因である例外</param>
+        public CommandException(string message, Exception innerException)
+            : base(message, innerException) { }
     }
 }
