82693e222633debf68bffa0d542ea626382e5d6f
h-saito <h-saito@powersolutions.co.jp>
2022-07-23 13:55:14 +0900

    add SimpleChatApp project

---
+++ src/DotNet6/SimpleChatApp/SimpleChatApp/Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "チャットアプリケーションのはじまり";
+            return View();
+        }
+    }
+}
---
+++ src/DotNet6/SimpleChatApp/SimpleChatApp/Program.cs
@@ -0,0 +1,21 @@
+namespace SimpleChatApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Chat}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
+    }
+}
